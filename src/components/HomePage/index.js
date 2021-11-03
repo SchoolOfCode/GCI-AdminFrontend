27aabf1b4fbd51684fe9fe7dca6699a901621394
Emanuel-David Bratasanu <davidthearchetype@gmail.com>
2021-11-03 16:50:38 +0000

    Small fix for Chris

--- src/components/HomePage/index.js
+++ src/components/HomePage/index.js
@@ -3,7 +3,6 @@ import ApplicationChart from "../ApplicationsChart";
 import StageChart from "../StageChart";
 import RegionChart from "../RegionChart";
 import GenderChart from "../GenderChart";
-import { Image } from "@chakra-ui/image";
 import { Text } from "@chakra-ui/layout";
 import { motion } from "framer-motion";
 
@@ -21,9 +20,8 @@ export default function HomePage() {
       <motion.img
         animate={{ scale: [0, 2, 1], rotate: 360 }}
         transition={{ duration: 1.5 }}
-        
         className="object-right"
-        src="https://ca.slack-edge.com/T6L933W4X-U6JL452LR-8b71d0aa850c-512"
+        src="https://i.ibb.co/cbQRNWK/chris.png"
         alt="chris"
       />
       <Text fontSize="3xl" fontFamily="fantasy">
