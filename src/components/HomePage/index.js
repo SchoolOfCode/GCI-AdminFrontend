0c33d1e27d5a30284a355c2173391c5d919ea295
Tommosaurus <87076987+Tommosaurus@users.noreply.github.com>
2021-11-03 16:41:51 +0000

    added image of Chris to the admin front page

--- src/components/HomePage/index.js
+++ src/components/HomePage/index.js
@@ -3,6 +3,9 @@ import ApplicationChart from "../ApplicationsChart";
 import StageChart from "../StageChart";
 import RegionChart from "../RegionChart";
 import GenderChart from "../GenderChart";
+import { Image } from "@chakra-ui/image";
+import { Text } from "@chakra-ui/layout";
+import { motion } from "framer-motion";
 
 export default function HomePage() {
   return (
@@ -15,6 +18,18 @@ export default function HomePage() {
         <RegionChart />
         <GenderChart />
       </section>
+      <motion.img
+        animate={{ scale: [0, 2, 1], rotate: 360 }}
+        transition={{ duration: 1.5 }}
+        
+        className="object-right"
+        src="https://ca.slack-edge.com/T6L933W4X-U6JL452LR-8b71d0aa850c-512"
+        alt="chris"
+      />
+      <Text fontSize="3xl" fontFamily="fantasy">
+        Hi Gang! Welcome to your admin page. Go forth and get us the best
+        candidates!
+      </Text>
     </div>
   );
 }
