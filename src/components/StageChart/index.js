004c42317fd4ca5af5bbec60865618017ec6fe22
Tommosaurus <87076987+Tommosaurus@users.noreply.github.com>
2021-11-04 14:11:45 +0000

    fixed graph issue with Loading state

--- src/components/StageChart/index.js
+++ src/components/StageChart/index.js
@@ -1,34 +1,41 @@
 import React, { useEffect, useState } from "react";
 import { Doughnut } from "react-chartjs-2";
 import { Box } from "@chakra-ui/react";
+import { Spinner } from "@chakra-ui/spinner";
 const axios = require("axios").default;
 
 export default function StageChart() {
+  const [isLoading, setLoading] = useState(true);
+  const [array, setArray] = useState([0, 0, 0, 0, 0, 0, 0]);
   let stages = [0, 0, 0, 0, 0, 0, 0];
 
-  async function getData() {
-    let response = await axios.get(`https://gci-backend.herokuapp.com/users/`);
+  useEffect(() => {
+    axios
+      .get(`https://gci-backend.herokuapp.com/users/`)
 
-    response.data.payload.forEach((item) => {
-      if (item.current_stage === 1) {
-        return (stages[0] += 1);
-      } else if (item.current_stage === 2) {
-        return (stages[1] += 1);
-      } else if (item.current_stage === 3) {
-        return (stages[2] += 1);
-      } else if (item.current_stage === 4) {
-        return (stages[3] += 1);
-      } else if (item.current_stage === 5) {
-        return (stages[4] += 1);
-      } else if (item.current_stage === 6) {
-        return (stages[5] += 1);
-      } else if (item.current_stage === 7) {
-        return (stages[6] += 1);
-      }
-    });
-    console.log("stages now", stages);
-  }
-  getData();
+      .then((response) => {
+        response.data.payload.forEach((item) => {
+          if (item.current_stage === 1) {
+            return (stages[0] += 1);
+          } else if (item.current_stage === 2) {
+            return (stages[1] += 1);
+          } else if (item.current_stage === 3) {
+            return (stages[2] += 1);
+          } else if (item.current_stage === 4) {
+            return (stages[3] += 1);
+          } else if (item.current_stage === 5) {
+            return (stages[4] += 1);
+          } else if (item.current_stage === 6) {
+            return (stages[5] += 1);
+          } else if (item.current_stage === 7) {
+            return (stages[6] += 1);
+          }
+        });
+        console.log("stages now", stages);
+        setArray(stages);
+        setLoading(false);
+      });
+  }, []);
 
   let data = {
     labels: [
@@ -42,7 +49,7 @@ export default function StageChart() {
     ],
     datasets: [
       {
-        data: stages,
+        data: array,
         backgroundColor: [
           "rgba(0, 0, 255, 0.5)",
           "rgba(60, 179, 133, 0.5)",
@@ -78,43 +85,30 @@ export default function StageChart() {
     },
   };
 
-  console.log("stages at render", stages);
-
-  if ((stages = [0, 0, 0, 0, 0, 0, 0])) {
-    return <p>Loading</p>;
-  } else {
+  if (isLoading) {
     return (
-      <Box
-        bg="white"
-        borderWidth="1px"
-        borderRadius="lg"
-        className="m-5 border-4 p-6 text-center  font-bold"
-      >
-        <div>
-          <div className="header">
-            <h1 className="title m-4 text-3xl">Applicant Stage</h1>
-          </div>
-          <Doughnut data={data} options={options} />
-        </div>
-      </Box>
+      <Spinner
+        thickness="4px"
+        speed="0.65s"
+        emptyColor="gray.200"
+        color="blue.500"
+        size="xl"
+      />
     );
   }
+  return (
+    <Box
+      bg="white"
+      borderWidth="1px"
+      borderRadius="lg"
+      className="m-5 border-4 p-6 text-center  font-bold"
+    >
+      <div>
+        <div className="header">
+          <h1 className="title m-4 text-3xl">Applicant Stage</h1>
+        </div>
+        <Doughnut data={data} options={options} />
+      </div>
+    </Box>
+  );
 }
-
-// const StageChart = () => (
-//   <Box
-//     bg="white"
-//     borderWidth="1px"
-//     borderRadius="lg"
-//     className="m-5 border-4 p-6 text-center  font-bold"
-//   >
-//     <div>
-//       <div className="header">
-//         <h1 className="title m-4 text-3xl">Applicant Stage</h1>
-//       </div>
-//       <Doughnut data={data} options={options} />
-//     </div>
-//   </Box>
-// );
-
-// export default StageChart;
